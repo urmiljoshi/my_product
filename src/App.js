a46fceecc4ff53de73307eecbb1b8ca095291594
urmiljoshi <urmiljoshi16@gmail.com>
2022-08-26 11:00:53 +0530

    26_8_22_new-Website

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Hedar from './Component/Hedar/Hedar';
 import Footer from './Component/Footer/Footer';
+import Home from './Cunteinar/Home/Home';
+import Detail from './Cunteinar/Detail/Detail';
+import Contact from './Cunteinar/Contact/Contact';
 
 function App() {
   return (
     <>
     <Hedar />
+    {/* <Home /> */}
+    {/* <Detail /> */}
+    <Contact />
     <Footer />
     </>
   );
